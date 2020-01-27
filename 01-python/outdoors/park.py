7bb30ab4d1b63db3c193394b57c7aba6415a81f5
alen <andrewsunup@gmail.com>
2020-01-27 17:29:46 -0500

    square park v1.0

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,23 @@
-def draw_park():
-    print("park not found")
+def draw_park(length = 5, width = 5):
+    #print("park not found")
+    #print (length)
+    #print (width)
+    print ("this is a park")
+    if length < 0 or width < 0:
+    	print("value error")
+    for i in range(length):
+    	if i == 0 or i == length - 1:
+    		print()
+    		for j in range(width):
+    			print("=", end = "")
+    			#print()
+    	else:
+    		print()
+    		for k in range(width):
+    			if k == 0 or k == width - 1:
+    				print("||", end = "")
+    			else:
+    				print(" ", end = "")
+
+    print()
     return
